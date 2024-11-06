c85c5f41083e1cf25cbe6dd0ae2ae02f98372f67
crubz11 <max@kaiser.family>
2024-11-06 09:33:55 +0100

    added grid system

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -1,58 +1,90 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
 namespace Game;
 
-public partial class Main : Node2D
+public partial class Main : Node
 {
 
 	private Sprite2D cursor;
 	private PackedScene buildingScene;
 	private Button placeBuildingButton;
+	private TileMapLayer highlightTilemapLayer;
+
+	private Vector2? hoveredGridCell;
+	private HashSet<Vector2> occupiedCells = new();
 
 	public override void _Ready()
 	{
 		buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
 		cursor = GetNode<Sprite2D>("Cursor");
 		placeBuildingButton = GetNode<Button>("PlaceBuildingButton");
+		highlightTilemapLayer = GetNode<TileMapLayer>("HighlightTileMapLayer");
 
 		cursor.Visible = false;
 
 		placeBuildingButton.Pressed += OnButtonPressed;
 	}
 
-    public override void _UnhandledInput(InputEvent evt)
-    {
-        if (cursor.Visible && evt.IsActionPressed("left_click"))
+	public override void _UnhandledInput(InputEvent evt)
+	{
+		if (hoveredGridCell.HasValue && evt.IsActionPressed("left_click") && !occupiedCells.Contains(hoveredGridCell.Value))
 		{
-			PalceBuildingAtMousePosition();
+			PalceBuildingAtHoveredCellPosition();
 			cursor.Visible = false;
 		}
-    }
+	}
 
 
-    public override void _Process(double delta)
+	public override void _Process(double delta)
 	{
 		var gridPosition = GetMouseGridCellPosition();
 		cursor.GlobalPosition = gridPosition * 64;
+		if (cursor.Visible && (!hoveredGridCell.HasValue || hoveredGridCell.Value != gridPosition))
+		{
+			hoveredGridCell = gridPosition;
+			UpdateHighlightTilemapLayer();
+		}
 	}
 
-	
+
 	private Vector2 GetMouseGridCellPosition()
 	{
-		var mousePosition = GetGlobalMousePosition();
+		var mousePosition = highlightTilemapLayer.GetGlobalMousePosition();
 		var gridPosition = mousePosition / 64;
 		gridPosition = gridPosition.Floor();
 		return gridPosition;
 	}
 
-	private void PalceBuildingAtMousePosition()
+	private void PalceBuildingAtHoveredCellPosition()
 	{
+		if (!hoveredGridCell.HasValue) return;
+
 		var building = buildingScene.Instantiate<Node2D>();
 		AddChild(building);
 
-		var gridPosition = GetMouseGridCellPosition();
-		building.GlobalPosition = gridPosition * 64;
+		building.GlobalPosition = hoveredGridCell.Value * 64;
+		occupiedCells.Add(hoveredGridCell.Value);
+
+		hoveredGridCell = null;
+		UpdateHighlightTilemapLayer();
+	}
+
+	private void UpdateHighlightTilemapLayer()
+	{
+		highlightTilemapLayer.Clear();
+		if (!hoveredGridCell.HasValue)
+		{
+			return;
+		}
+
+		for (var x = hoveredGridCell.Value.X - 3; x <= hoveredGridCell.Value.X + 3; x++)
+		{
+			for (var y = hoveredGridCell.Value.Y - 3; y <= hoveredGridCell.Value.Y + 3; y++)
+			{
+				highlightTilemapLayer.SetCell(new Vector2I((int)x, (int)y), 0, Vector2I.Zero);
+			}
+		}
 	}
 
 	private void OnButtonPressed()
